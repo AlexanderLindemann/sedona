126b03ca1c582128d68b8dd46b8f39258ff9cfb5
aleksandrlindemann <idsmokedried@gmail.com>
2022-04-04 14:54:18 +0300

    Ук

---
+++ src/main/java/com/losos/sedona/SedonaApplication.java
@@ -0,0 +1,13 @@
+package com.losos.sedona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SedonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SedonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/losos/sedona/SedonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.losos.sedona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SedonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
